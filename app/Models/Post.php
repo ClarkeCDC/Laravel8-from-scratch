de688bd025d9c1d147a8e5d4513eb60c52f4f4bd
Christian Clarke <christian_clarke@icloud.com>
2021-08-19 17:16:39 +0100

    Created and implemented post class

---
+++ app/Models/Post.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+
+class Post {
+
+    public static function find($slug){
+        if (!file_exists($path = resource_path("posts\\".$slug.".html"))){
+            throw new ModelNotFoundException();
+        }
+        // use (path) passes through variable so it accesiblle   
+        return cache()->remember("posts.{$slug}", 1200, function () use($path) { 
+            file_get_contents($path);
+        });
+    }
+}
