f79894e8aa57c553a46d679e434f04c80c232738
Christian Clarke <christian_clarke@icloud.com>
2021-08-20 16:34:14 +0100

    remove breeze

--- routes/web.php
+++ routes/web.php
@@ -17,10 +17,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth'])->name('dashboard');
-
 Route::get('/posts', function () {
     return view ('posts', [
         'posts' => Post::all()
@@ -33,4 +29,3 @@ Route::get('/posts/{post}', function ($slug) {
     ]);
 });
 
-require __DIR__ . '/auth.php';
